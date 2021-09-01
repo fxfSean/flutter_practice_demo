8fd9cef03822d1fb31ecabfd041a8a9dc7510b2a
fuxiaofang <fuxiaofang@360.cn>
2021-09-01 10:33:01 +0800

    切换page保留状态

--- lib/page/first_page.dart
+++ lib/page/first_page.dart
@@ -2,13 +2,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> with AutomaticKeepAliveClientMixin {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('First'),
+      body: GestureDetector(
+        onTap: (){
+          setState(() {
+            count++;
+          });
+        },
+        child: Center(
+          child: Text('First $count'),
+        ),
       ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
